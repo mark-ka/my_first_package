45f4f0d90faa4c7d3fa9552835c2b1e53cacbd02
Markus Kramer <kramer.markus@posteo.de>
2022-02-21 15:41:55 +0100

    Initial Push

---
+++ project_X/lib.py
@@ -0,0 +1,9 @@
+import math
+
+def try_me():
+    return f'Piiiii -{math.pi}- \nshould be infinityyyy!!\n✨🔮🪄🌟💫 \nbut it seems to be rounded here..\n :((('
+
+
+
+if __name__ == '__main__':
+    print(try_me())
